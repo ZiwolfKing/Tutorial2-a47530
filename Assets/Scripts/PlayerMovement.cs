9e1d828eb75808a630262e91f12a9c66b5ada1fd
ZiwolfKing <a47530@alunos.ipb.pt>
2022-05-04 19:47:12 +0100

    9.Get a reference to the Animator component

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Animator m_Animator;
     Vector3 m_Movement;
 
-    // Start is called before the first frame update
-    void Start()
+    void Start ()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
         float horizontal = Input.GetAxis ("Horizontal");
         float vertical = Input.GetAxis ("Vertical");
-
+        
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize ();
+
         bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
     }
 }
+
 

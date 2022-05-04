fab3de56981fc19f3963bd1d18a6e68fb8df2d70
ZiwolfKing <a47530@alunos.ipb.pt>
2022-05-04 19:40:51 +0100

    4.How Did You Create That Variable?

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,5 +15,6 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis ("Horizontal");
+        float vertical = Input.GetAxis ("Vertical");
     }
 }

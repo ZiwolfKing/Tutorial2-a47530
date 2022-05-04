a7336831aa231939ef96f80cdd8fcc270a820a2c
ZiwolfKing <a47530@alunos.ipb.pt>
2022-05-04 19:52:51 +0100

    13.Apply Movement and Rotation to your Character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,13 @@ public class PlayerMovement : MonoBehaviour
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
+    Rigidbody m_Rigidbody;
+    
+
     void Start ()
     {
         m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody> ();
     }
 
     void Update ()
@@ -30,6 +34,11 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
+    }
+
+    void OnAnimatorMove ()
+    {
+
     }
 }
 
